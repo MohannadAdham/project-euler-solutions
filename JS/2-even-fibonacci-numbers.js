1f2c674f43803e542fea14a0b8e88df18dae8246
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-29 22:34:42 +0100

    Add a solution to problem 2 in JS.

---
+++ JS/2-even-fibonacci-numbers.js
@@ -0,0 +1,14 @@
+/* Problem: By considering the terms in the Fibonacci sequence 
+whose values do not exceed n, find the sum of the even-valued terms.*/
+
+function fiboEvenSum(n) {
+    let sum = 0;
+    let prevNum = 1;
+    let febNum = 2;
+
+    while (febNum <= n) {
+      if (febNum % 2 == 0) sum += febNum;
+      [prevNum, febNum] = [febNum, febNum + prevNum];
+    }
+  return sum;
+}
